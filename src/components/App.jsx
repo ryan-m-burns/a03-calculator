bb291d1c380bfaa24ace1c0e6f6965a97e5b6e25
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-01-17 17:04:54 -0800

    import button assets'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { useState } from 'react';
 
 import '../styles/App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [secondNumber, setSecondNumber] = useState('');
+  const [operator, setOperator] = useState('');
 
   return (
-    <>
+    <div>
       <h1>Hello World</h1>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/CalculatorButton.jsx
@@ -0,0 +1,8 @@
+function CalculatorButton({ }) {
+
+  return (
+    <button></button>
+  )
+}
+
+export default CalculatorButton;
